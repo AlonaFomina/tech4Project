64bb53017bd2661d14db7dd3101dfdfebb8065a8
AFomina <alonafomina3@gmail.com>
2022-06-17 15:22:49 -0500

    added interview question

--- src/homeworks/Homework22.java
+++ src/homeworks/Homework22.java
@@ -9,7 +9,7 @@ public class Homework22 {
 
     public static int[] fibonacciSeries1(int num) {
         int arr[] = new int[num];
-        if (num >= 1) arr[0] = 1;
+        if (num >= 1) arr[0] = 0;
         if (num >= 2) arr[1] = 1;
 //        int num1 = 1;
 //        int num2 = 1;
